da823337560c4af88ed9b9dc1f17cd0dfcfb9e5f
kohan <hanke0@outlook.com>
2019-04-11 22:32:25 +0800

    improve setup

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup, Extension
 
-DateExt = [
+extensions = [
     Extension("ctools", ["ctoolsmodule.c"]),
 ]
 
 setup(
     name="ctools",
     version="0.0.1",
-    ext_modules=DateExt
+    ext_modules=extensions,
+    description="A toolbox for python implement in python.",
+    url="https://github.com/ko-han/python-ctools",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
