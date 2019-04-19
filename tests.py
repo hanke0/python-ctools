441e24aca17a8d8f151fa2d53143544b21ea54cd
kohan <hanke0@outlook.com>
2019-04-19 23:06:00 +0800

    more test

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 import unittest
 import random
 import string
+import uuid
+import sys
+import gc
 from datetime import datetime, timedelta
 
 import ctools
@@ -46,23 +49,67 @@ class T(unittest.TestCase):
         for i in range(1024):
             self.assertEqual(ctools.strhash(s), a)
 
-    def test_lfu(self):
+
+class LFUTest(unittest.TestCase):
+    def test_get_set(self):
         d = ctools.LFUCache(2)
         d['a'] = 1
         d['c'] = 2
         d['e'] = 3
         self.assertEqual(len(d), 2)
 
-        for i in range(1024):
+        for i in range(10):
             self.assertEqual(d['c'], 2)
 
         self.assertEqual(d.lfu(), "e")
 
-        d.set_capacity(1024)
-        for i in range(2048):
-            d[random.random()] = random.random()
+    def test_len(self):
+        for m in range(254, 1024):
+            d = ctools.LFUCache(m)
+            for i in range(m+1):
+                d[str(i)] = str(i)
 
-        self.assertEqual(len(d), 1024)
+            self.assertEqual(len(d), m)
+
+    def assert_ref_equal(self, lfu_cache, lkey, d, dkey):
+        ltmp = lfu_cache[lkey]
+        dtmp = d[dkey]
+        self.assertEqual(sys.getrefcount(ltmp), sys.getrefcount(dtmp))
+
+        del lfu_cache[lkey]
+        del d[dkey]
+        self.assertEqual(sys.getrefcount(ltmp), sys.getrefcount(dtmp))
+
+        del d
+        del lfu_cache
+        self.assertEqual(sys.getrefcount(ltmp), sys.getrefcount(dtmp))
+
+    def set_random(self, dlike):
+        key = str(uuid.uuid1())
+        val = str(uuid.uuid1())
+        dlike[key] = val
+        return key
+
+    def test_one_ref_eq(self):
+        lfu_cache = ctools.LFUCache(10)
+        d = {}
+
+        lkey = self.set_random(lfu_cache)
+        dkey = self.set_random(d)
+
+        self.assert_ref_equal(lfu_cache, lkey, d, dkey)
+
+    def test_many_ref_eq(self):
+        lfu_cache = ctools.LFUCache(257)
+        d = {}
+        for i in range(1024):
+            self.set_random(lfu_cache)
+            self.set_random(d)
+
+        for i in range(len(lfu_cache)):
+            lkey = list(lfu_cache.keys())[i]
+            dkey = list(d.keys())[i]
+            self.assert_ref_equal(lfu_cache, lkey, d, dkey)
 
 
 if __name__ == '__main__':
