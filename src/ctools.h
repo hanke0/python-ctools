d11bc82c637d2c360db2aa7e9623ca8171c1a36e
ko-han <hanke0@outlook.com>
2019-04-27 00:02:38 +0800

    add file header

--- src/ctools.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CTOOLS_H
-#define _CTOOLS_H
-
-#include <Python.h>
-#include "ctools_compt.h"
-
-#endif  // _CTOOLS_H
--- src/ctools_funcs.h
+++ src/ctools_funcs.h
@@ -1,8 +1,22 @@
+/* Copyright 2019 ko-han. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 #ifndef _CTOOLS_FUNCS_H
 #define _CTOOLS_FUNCS_H
 #include <Python.h>
 #include <datetime.h>
-#include "ctools.h"
+#include "ctools_config.h"
 
 PyDoc_STRVAR(jump_consistent_hash__doc__,
              "jump_consistent_hash(key: int, num_buckets: int) -> int:\n\n\
