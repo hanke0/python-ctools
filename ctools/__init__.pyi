aecdc50f6e2ec882214222caefcb11509bc1a4b8
ko-han <hanke0@outlook.com>
2019-07-06 03:12:12 +0800

    ttlcache doc

--- ctools/__init__.pyi
+++ ctools/__init__.pyi
@@ -32,7 +32,7 @@ def int8_to_datetime(date_integer: int) -> datetime: ...
 #- api
 #- description: A cache Map behaving like dict
 class CacheMap:
-
+    """New in version 0.1.0"""
     def __init__(self, capacity: int) -> None:
         """
         :param capacity: max items cache would hold.
@@ -116,6 +116,96 @@ class CacheMap:
         """
 
     def next_evict_key(self) -> Any: ...
+
+    def setnx(self, key, callback: Callable[[], Any]):
+        """
+        Insert key ith a value of callback() if key is not in the dictionary.
+
+        Return the value for key if key is in the dictionary, else callback().
+        """
+        pass
+
+
+#- api
+#- description: A cache map stores items in certain seconds and behaves much like dict.
+class TTLCache:
+    """New in version 0.1.0"""
+    def __init__(self, ttl: int) -> None:
+        """
+        :param ttl: max seconds a items stores.
+        """
+        pass
+
+    def get(self, key, default=None):
+        """ Return the value for key if key is in the cache, else default. """
+        pass
+
+    def pop(self, key, default=None): # real signature unknown; restored from __doc__
+        """
+        Cache.pop(k[,default]) -> v, remove specified key and return the corresponding value.
+        If key is not found, default is returned
+        """
+        pass
+
+    def setdefault(self, *args, **kwargs): # real signature unknown
+        """
+        Insert key with a value of default if key is not in the dictionary.
+
+        Return the value for key if key is in the dictionary, else default.
+        """
+        pass
+
+    def update(self, mp: Optional[Mapping] = None, **kwargs): # known special case of dict.update
+        """
+        Cache.update([mp, ]**kwargs) -> None.  Update Cache from dict/iterable mp and kwargs.
+        If mp is present then does:  for k in mp: Cache[k] = mp[k]
+        In either case, this is followed by: for k in kwargs:  Cache[k] = kwargs[k]
+        """
+        pass
+
+    def keys(self) -> Iterable:
+        """Return key list."""
+        pass
+
+    def values(self) -> Iterable:
+        """Return value list."""
+        pass
+
+    def items(self) -> Iterable[Tuple]:
+        """Return (k, v) pairs list."""
+        pass
+
+    def clear(self):
+        """ Cache.clear() -> None.  Remove all items from Cache. """
+        pass
+
+    def __contains__(self, key): # real signature unknown
+        """ True if the dictionary has the specified key, else False. """
+        pass
+
+    def __delitem__(self, key): # real signature unknown
+        """ Delete self[key]. """
+        pass
+
+    def __setitem__(self, key, value): # real signature unknown
+        """ self[key] = value. """
+        pass
+
+    def __getitem__(self, key): # real signature unknown; restored from __doc__
+        """ Return self[key] """
+        pass
+
+    def __len__(self, *args, **kwargs): # real signature unknown
+        """ Return len(self). """
+        pass
+
+    def set_default_ttl(self, ttl: int) -> None:
+        """reset default ttl"""
+        ...
+
+    def get_default_ttl(self) -> int:
+        """Return default ttl"""
+        pass
 
     def setnx(self, key, callback: Callable[[], Any]):
         """
