34c0ed26d069ca18f7331c286cfa12233e1b225a
ko-han <hanke0@outlook.com>
2019-07-09 02:06:15 +0800

    simple install

--- tools/install.sh
+++ tools/install.sh
@@ -2,18 +2,8 @@
 
 HERE=$(dirname $0)
 PROJECT_ROOT=$(dirname ${HERE})
-VERSION_FILE=${PROJECT_ROOT}/ctools/version.py
 
-old_version=$(cat ${VERSION_FILE})
-
-if [[ ${CTOOLS_DEBUG} == "ON" ]]; then
-    echo "write git commit version info to " ${VERSION_FILE}
-    python ${HERE}/githeadversion.py
-fi
 pip -V
-pip install . -v
+echo pip install ${PROJECT_ROOT} -v
+pip install ${PROJECT_ROOT} -v
 
-echo "Restore version file" ${VERSION_FILE}
-cat > ${VERSION_FILE} <<EOF
-${old_version}
-EOF
