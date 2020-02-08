6fd4c7802c1e9953ba7c463da30d0011cd962fe6
ko-han <hanke0@outlook.com>
2020-02-08 22:26:57 +0800

    use python unittest as test tool

--- Makefile
+++ Makefile
@@ -1,38 +1,54 @@
-.DEFAULT_GOAL := help
+.DEFAULT_GOAL := build
 
 # Absolutely awesome: http://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
+.PHONY: help
 help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
+.PHONY: dist
 dist:  ## Build dists
 	@$(CURDIR)/tools/clean-cache.sh
 	@$(CURDIR)/tools/dist.sh
 	@$(CURDIR)/tools/clean-cache.sh soft
 
+.PHONY: clean
 clean:  ## Clean cache, include dist
-	@$(CURDIR)/tools/clean-cache.sh
+	find -name *.egg-info -exec rm -rf {} +
+	find -name *.pyc -exec rm -rf {} +
+	find -name *.pyo -exec rm -rf {} +
+	find -name *.so -exec rm -rf {} +
+	rm -rf dist
+	rm -rf build
 
+.PHONY: format
 format:  ## Format code
-	@clang-format -style=Mozilla -i src/*.h
-	@clang-format -style=Mozilla -i src/*.c
+	@clang-format --verbose -style=LLVM -i src/*
 	@black **/*.py
 
+.PHONY: benchmark
 benchmark:  ## Run benchmark
 	@$(CURDIR)/tools/runbenchmark.py benchmarks/
 
+.PHONY: pypi-test
 pypi-test:
 	@$(CURDIR)/tools/upload-pypi.sh test
 
+.PHONY: pypi
 pypi:
 	@$(CURDIR)/tools/upload-pypi.sh
 
-build:  ## build package
+.PHONY: build
+build: clean  ## build package
 	@python setup.py build_ext --inplace
 
-test: build  ## Install package and Test
-	@$(CURDIR)/tools/runtest.py
+.PHONY: test
+test:  ## running test
+	@$(CURDIR)/tools/runtest.py -s ./tests -p .
 
-doc: build  ## genetate api doc
+.PHONY: doc
+doc: build ## genetate api doc
 	cd docs && make clean && make html
 
-.PHONY: help dist clean format benchmark pypi-test pypi build test doc
+.PHONY: check
+check:
+	clang-tidy src/* -- -I/usr/include/python3.7m/
--- tools/runtest.py
+++ tools/runtest.py
@@ -2,7 +2,18 @@
 import sys
 import os
 import shutil
-import pytest
+import unittest
+import argparse
+
+info = """
+Testing Information
+    Python： %(py)s
+    PythonVer: %(pyver)s
+    Version: %(version)s
+    DebugBuild: %(debug)s
+    PackageDir：%(package_path)s
+    WorkingDir: %(path)s
+"""
 
 
 def _show_info():
@@ -10,33 +21,59 @@ def _show_info():
 
     separate = "-" * shutil.get_terminal_size()[0]
     print(separate)
-    print("Testing Information")
-    print("\t", "Python: ", sys.executable)
     print(
-        "\t",
-        "Ctools:",
-        ctools.__version__,
-        "(debug build)" if ctools.build_with_debug() else "",
-        "at %s" % ctools.__file__,
+        info
+        % dict(
+            py=sys.executable,
+            pyver=sys.version.replace("\n", " "),
+            version=ctools.__version__,
+            debug="true" if ctools.build_with_debug() else "false",
+            package_path=ctools.__file__,
+            path=os.getcwd(),
+        )
     )
     print(separate)
 
 
-def _setup_project_root():
-    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sys.path.pop(0)
-    sys.path.insert(0, project_root)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-k", dest="pattern", help="Only run tests which match the given substring"
+)
+parser.add_argument("-v", "--verbose", dest="verbose", help="Verbose output")
+parser.add_argument("-q", "--quiet", dest="quiet", help="Quiet output")
+parser.add_argument(
+    "-s",
+    "--start-directory",
+    default=".",
+    dest="start_directory",
+    help="Directory to start discovery ('.' default)",
+)
+parser.add_argument(
+    "-p",
+    "--python-path",
+    dest="python_path",
+    action="append",
+    help="Include python path",
+)
 
 
-def main(argv=None):
-    if argv is not None and "--no-project-root" in argv:
-        argv.remove("--no-project-root")
-    else:
-        _setup_project_root()
+def main():
+    args = parser.parse_args()
+    if args.python_path:
+        for i in args.python_path:
+            sys.path.insert(-2, i)
 
+    argv = [sys.argv[0], "discover", "-s", args.start_directory]
+    if args.verbose:
+        argv.append("-v")
+    if args.quiet:
+        argv.append("-q")
+    if args.pattern:
+        argv.append("-k")
+        argv.append(args.pattern)
     _show_info()
     try:
-        code = pytest.main(argv)
+        code = unittest.main(module=None, argv=argv)
     except SystemExit as exc:
         code = exc.code
 
